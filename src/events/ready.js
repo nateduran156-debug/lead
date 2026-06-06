ff57c59a729d4ad2e30dddd91dcb4ff8f227d650
ddi=d <nateduran156@gmail.com>
2026-06-06 15:01:31 -0700

    Add files via upload

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,21 +1,10 @@
-import { ActivityType } from 'discord.js';
-import { startSniperLoop } from '../handlers/sniperHandler.js';
-import { startReminderLoop } from '../handlers/reminderHandler.js';
-import { startGiveawayLoop } from '../handlers/giveawayHandler.js';
+const logger = require('../utils/logger');
 
-export const name = 'ready';
-export const once = true;
-
-export async function execute(client) {
-  console.log(`✅ Logged in as ${client.user.tag}`);
-  console.log(`📊 Serving ${client.guilds.cache.size} guilds | ${client.users.cache.size} users`);
-
-  client.user.setPresence({
-    activities: [{ name: '@unheardlust', type: ActivityType.Watching }],
-    status: 'idle',
-  });
-
-  startSniperLoop(client);
-  startReminderLoop(client);
-  startGiveawayLoop(client);
-}
+module.exports = {
+  name: 'ready',
+  once: true,
+  execute(client) {
+    logger.info(`Online as ${client.user.tag}`);
+    client.user.setActivity({ name: 'Watching', type: 3 });
+  }
+};
