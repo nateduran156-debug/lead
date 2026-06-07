528c67680809d7ad12df8756703aa3e18f666756
ddi=d <nateduran156@gmail.com>
2026-06-07 10:48:18 -0700

    Add files via upload

--- events/ticketButton.js
+++ events/ticketButton.js
@@ -1,12 +1,16 @@
-import { getGuild, createTicket, getTicket, closeTicket } from '../utils/database.js';
-import { ok, err, CV2, COLORS } from '../utils/components.js';
-import {
+'use strict';
+
+const { getGuild, getTicket, closeTicket, openTicket } = require('../utils/database');
+const { ok, err, COLORS } = require('../utils/components');
+const {
   ChannelType, PermissionFlagsBits, ActionRowBuilder, ButtonBuilder,
   ButtonStyle, ContainerBuilder, TextDisplayBuilder, SeparatorBuilder,
   SeparatorSpacingSize, MessageFlags,
-} from 'discord.js';
+} = require('discord.js');
 
-export async function handleTicketCreate(interaction, client) {
+const CV2 = MessageFlags.IsComponentsV2;
+
+async function handleTicketCreate(interaction, client) {
   await interaction.deferReply({ ephemeral: true });
   const guild = interaction.guild;
   const g = getGuild(guild.id);
@@ -26,7 +30,7 @@ export async function handleTicketCreate(interaction, client) {
     ],
   });
 
-  createTicket(guild.id, channel.id, interaction.user.id);
+  openTicket(guild.id, channel.id, interaction.user.id);
 
   const container = new ContainerBuilder()
     .setAccentColor(COLORS.blue)
@@ -39,11 +43,11 @@ export async function handleTicketCreate(interaction, client) {
     new ButtonBuilder().setCustomId('ticket_close').setLabel('Close Ticket').setEmoji('🔒').setStyle(ButtonStyle.Danger),
   );
 
-  await channel.send({ content: `${interaction.user}`, flags: MessageFlags.IsComponentsV2, components: [container, closeBtn] });
+  await channel.send({ content: `${interaction.user}`, flags: CV2, components: [container, closeBtn] });
   await interaction.editReply(ok(`ticket created: ${channel}`));
 }
 
-export async function handleTicketClose(interaction, client) {
+async function handleTicketClose(interaction, client) {
   await interaction.deferReply({ ephemeral: true });
   const ticket = getTicket(interaction.channel.id);
   if (!ticket) return interaction.editReply(err('this is not an open ticket'));
@@ -51,7 +55,9 @@ export async function handleTicketClose(interaction, client) {
   closeTicket(interaction.channel.id);
   const c = new ContainerBuilder().setAccentColor(COLORS.red)
     .addTextDisplayComponents(new TextDisplayBuilder().setContent('🔒 ticket closed, deleting in 5 seconds'));
-  await interaction.channel.send({ flags: MessageFlags.IsComponentsV2, components: [c] });
+  await interaction.channel.send({ flags: CV2, components: [c] });
   await interaction.editReply(ok('ticket closed'));
   setTimeout(() => interaction.channel.delete().catch(() => {}), 5000);
 }
+
+module.exports = { handleTicketCreate, handleTicketClose };
